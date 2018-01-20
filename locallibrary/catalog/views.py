0271279a2f79a0df53ecd4b6ec18bc11ecae2198
roman@kislovs.ru <Rkislov2006>
2018-01-20 23:39:40 +0500

    Add index page

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance, Genre
 
 # Create your views here.
+
+def index(request):
+    """
+    View function for home page of site
+    """
+    #Generate counts of some of the main objects
+
+    num_books=Book.objects.all().count()
+    num_instances=BookInstance.objects.all().count()
+
+    # Available books (ststus = 'a')
+
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+    num_authors= Author.objects.count()
+    num_genres = Genre.objects.count()
+
+    return render(request, 'index.html', context={'title':'Домашняя страница','num_books':num_books,'num_authors': num_authors, 'num_instances': num_instances,'num_instances_availible':num_instances_available, 'num_genres': num_genres})
+
