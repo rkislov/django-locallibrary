640c27185bb05844d8c57aead590fd1badcf6227
roman@kislovs.ru <Rkislov2006>
2018-01-20 19:57:28 +0500

    completed admin site

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -3,8 +3,43 @@ from .models import Author, Genre, Book, BookInstance, Language
 
 # Register your models here.
 
-admin.site.register(Book)
-admin.site.register(Author)
+#admin.site.register(Book)
+#admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+#admin.site.register(BookInstance)
 admin.site.register(Language)
+
+class BookInline(admin.TabularInline):
+    model = Book
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BookInline]
+
+admin.site.register(Author, AuthorAdmin)
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'display_genre', 'language')
+    inlines = [BookInstanceInline]
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_display = ('book', 'status', 'due_back', 'id')
+    list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Доступность', {
+            'fields': ('status', 'due_back')
+        }),
+        
+    )
--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -3,7 +3,7 @@ import uuid
 
 # Create your models here.
 class Genre(models.Model):
-    name = models.CharField(max_length=200, help_text="Введите жанр книги (напр. Фантастика, Детектив, Роман)")
+    name = models.CharField('Название',max_length=200, help_text="Введите жанр книги (напр. Фантастика, Детектив, Роман)")
     
     def __str__(self):
         return self.name
@@ -11,7 +11,7 @@ class Genre(models.Model):
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-    summary = models.TextField(max_length=1000, help_text="Введите описание книги")
+    summary = models.TextField('Описание',max_length=1000, help_text="Введите описание книги")
     isbn = models.CharField('ISBN', max_length=13, help_text='13 символов <a href="https://www.isbn-international.org/content/what-isbn">ISBN номер</a>')
     genre = models.ManyToManyField(Genre, help_text="Выберите жанр для книги")
     language = models.ForeignKey('Language', on_delete=models.SET_NULL, null=True)
@@ -21,6 +21,11 @@ class Book(models.Model):
 
     def get_absolut_url(self):
         return reverse('book-detail', args=[str(self.id)])
+
+    def display_genre(self):
+        return ', '.join([genre.name for genre in self.genre.all()[:3] ])
+    display_genre.short_description = 'Жанр'
+    author.short_description = 'Автор'
 
 class BookInstance(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, help_text="Угикальный номер для издания")
@@ -44,10 +49,10 @@ class BookInstance(models.Model):
         return '{0} ({1})'.format(self.id, self.book.title)
 
 class Author(models.Model):
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    date_of_birth = models.DateField(null=True, blank=True)
-    date_of_death = models.DateField('Died', blank=True, null=True)
+    first_name = models.CharField('Имя', max_length=100)
+    last_name = models.CharField('Фамилия',max_length=100)
+    date_of_birth = models.DateField('Родился',null=True, blank=True)
+    date_of_death = models.DateField('Умер', blank=True, null=True)
 
     class Meta:
         ordering = ["last_name", "first_name"]
@@ -61,7 +66,7 @@ class Author(models.Model):
         return '{0}, {1}'.format(self.last_name, self.first_name)
 
 class Language(models.Model):
-    name = models.CharField(max_length=200, help_text="Введите язык на котором написанна книга")
+    name = models.CharField('Язык',max_length=200, help_text="Введите язык на котором написанна книга")
     
     def __str__(self):
         return self.name
